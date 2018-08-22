96ab1e2372a39e8a034346ccf377ceeffc465b38
rezamike <michael.sanaiha@gmail.com>
2018-08-21 22:32:27 -0700

    getting that java down

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var win = 1
-var loss = 1
+var loss = 0
 var guessLeft = 9
 var winning = document.getElementById("wins");
 var losing = document.getElementById("losses");
@@ -11,17 +11,30 @@ document.onkeyup = function (event) {
     var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
     var user = document.getElementById("answers");
 
+    balls.textContent = ("Guesses: " + "answers");
+
     if (computerGuess === balls) {
         winning.textContent = (win++);
     }
 
     else {
-        losing.textContent = (loss++);
         guessing.textContent = (guessLeft--);
     }
 
     if (guessLeft === 0) {
+        alphabet[Math.floor(Math.random() * alphabet.length)];
         guessLeft = 9;
-        loss = 1;
+        losing.textContent = (loss++);
     }
+
+    console.log(balls);
+console.log("------------");
+    console.log(loss);
+    console.log("------------");
+    console.log(guessLeft);
+    console.log("------------");
+    console.log(win);
+    console.log("------------");
+    console.log(computerGuess);
+    console.log("------------");
 }
