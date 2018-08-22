dcbed4e2b4c2d4b7705917607bded6733f330cca
rezamike <michael.sanaiha@gmail.com>
2018-08-21 18:20:57 -0700

    kind of fixed some minor issues with the wins/losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,23 @@
-<script>
-    
-</script>
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var win = 1
+var loss = 1
+var guessLeft = 9
+var winning = document.getElementById("wins");
+var losing = document.getElementById("losses");
+var guessing = document.getElementById("guess");
+
+document.onkeyup = function (event) {
+    var balls = event.key;
+    var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    if (computerGuess === balls) {
+        winning.innerHTML = ("Wins: " + win++);
+    }
+
+
+
+    console.log(alphabet);
+    console.log(computerGuess);
+    console.log(balls);
+    console.log(winning)
+}
