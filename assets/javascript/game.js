39a64bf1256dd5fa0dda466ea424135be8314aee
rezamike <michael.sanaiha@gmail.com>
2018-08-21 18:50:04 -0700

    missing quite a few pieces, but making good headway

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var win = 1
-var loss = 1
+var win = 0
+var loss = 0
 var guessLeft = 9
 var winning = document.getElementById("wins");
 var losing = document.getElementById("losses");
@@ -9,15 +9,14 @@ var guessing = document.getElementById("guess");
 document.onkeyup = function (event) {
     var balls = event.key;
     var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+    var user = document.getElementById("answers");
 
     if (computerGuess === balls) {
-        winning.innerHTML = ("Wins: " + win++);
+        winning.textContent = (win++);
     }
 
-
-
-    console.log(alphabet);
-    console.log(computerGuess);
-    console.log(balls);
-    console.log(winning)
+    else {
+        losing.textContent = (loss++);
+        guessing.textContent = (guessLeft--);
+    }
 }
